c80b00db5acaf1f4cd3030391abaf7fd5a964ee1
Josh Mattingly <josh.mattingly@conagra.com>
2021-10-06 20:04:31 -0500

    Data aggregation test

--- dataFuser.py
+++ dataFuser.py
@@ -3,6 +3,9 @@ import pandas as pd
 from acaParser import process_images
 from neoParser import process_neo
 from calipsoParser import process_sat
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 
 dir_anom = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/temp_anom'
@@ -16,6 +19,26 @@ bound_right = -79.850692749023
 bound_bottom = 24.208717346191
 
 df_anom = process_images(dir_anom, 'temp_anom', bound_top, bound_left, bound_bottom, bound_right, False)
-df_coral = process_images(dir_coral, 'coral_fungi', bound_top, bound_left, bound_bottom, bound_right, True)
+df_coral = process_images(dir_coral, 'coral_algae', bound_top, bound_left, bound_bottom, bound_right, True)
 df_neo = process_neo(dir_neo, bound_top, bound_left, bound_bottom, bound_right)
 df_sat = process_sat(dir_calipso)
+
+
+def aggregate_data(df_main, metric):
+    df = df_main
+    # df = df[df.temp_anom > 0]
+    df['Long'] = np.round(df['Long'], 3)
+    df['Lat'] = pd.to_numeric(df.Lat)
+    df['Lat'] = np.round(df.Lat, 3)
+    return df.groupby(['Long', 'Lat', 'Date']).agg(X=(metric, 'median')).reset_index()
+
+
+df_anom_copy = aggregate_data(df_anom, 'temp_anom')
+df_coral_copy = aggregate_data(df_coral, 'coral_algae')
+df_neo_copy = aggregate_data(df_neo, 'chlorophyll')
+
+
+test_set_full = df_anom[df_anom.Date == '2020-02-21']
+plt.scatter(x=test_set_full.Lat, y=test_set_full.Long, c=test_set_full.temp_anom)
+test_set_abbr = aggregate_data(test_set_full, 'temp_anom')
+plt.scatter(x=test_set_abbr.Lat, y=test_set_abbr.Long, c=test_set_abbr.X)
