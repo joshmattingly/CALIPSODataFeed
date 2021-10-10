9682bda0a5698a59b858c084a6222fd321f64a37
Josh Mattingly <josh.mattingly@conagra.com>
2021-10-10 14:23:12 -0500

    process feature  images

--- acaParser.py
+++ acaParser.py
@@ -46,14 +46,57 @@ def process_images(dir, column, top, left, bottom, right, coral=False):
     return df
 
 
+def process_geos(dir, top, left, bottom, right):
+    dir_listing = os.listdir(dir)
+    df = None
+    for file in dir_listing:
+        if ".png" in file:
+            print("Processing {}".format(file))
+            datestamp = re.findall("(\d{2}\D{3}\d{4})", file)[0]
+            feature = re.findall("(\w*)_", file)[0]
+
+            img = cv2.imread('{}/{}'.format(dir, file))
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray[gray == 19] = 0
+            # if detecting coral/fungi, flag sets response to boolean
+
+            thresh = cv2.threshold(gray, 100, 200, cv2.THRESH_TOZERO_INV)[1]
+            border = np.where(thresh == 0)
+            top_thickness = 5
+            bottom_thickness = 6
+            top_left = (np.min(border[0])+top_thickness, np.min(border[1])+top_thickness)
+            bottom_right = (np.max(border[0])-bottom_thickness, np.max(border[1])-bottom_thickness)
+            cropped = gray[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+            y, x = cropped.shape
+            long_increment = (bottom - top) / y
+            lat_increment = (right - left) / x
+            lat_range = list(np.arange(left, right, lat_increment))
+            long_range = list(np.arange(top, bottom, long_increment))
+            if df is None:
+                df = pd.DataFrame(cropped, index=long_range, columns=lat_range)
+                df = pd.melt(df.reset_index(), id_vars='index')
+                df.columns = ['Lat', 'Long', feature]
+                df['Date'] = pd.to_datetime(datestamp, format="%d%b%Y")
+            else:
+                while cropped.shape[1] < len(lat_range):
+                    lat_range.pop()
+                df_temp = pd.DataFrame(cropped, index=long_range, columns=lat_range)
+                df_temp = pd.melt(df_temp.reset_index(), id_vars='index')
+                df_temp.columns = ['Lat', 'Long', feature]
+                df_temp['Date'] = pd.to_datetime(datestamp, format="%d%b%Y")
+                df = pd.concat([df, df_temp])
+    return df
+
+
 if __name__ == "__main__":
     # test points from CALIPSO_ACA_Subsetter
     dir_anom = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/temp_anom'
     dir_coral = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/coral_snapshot'
+    dir_geo = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/geo_data'
     top = 26.405982971191
     left = -82.882919311523
     right = -79.850692749023
     bottom = 24.208717346191
 
-    df_anom = process_images(dir_anom, 'temp_anom', top, left, bottom, right, False)
-    df_coral = process_images(dir_coral, 'coral_fungi', top, left, bottom, right, True)
+    # df_anom = process_images(dir_anom, 'temp_anom', top, left, bottom, right, False)
+    # df_coral = process_images(dir_coral, 'coral_fungi', top, left, bottom, right, True)
--- dataFuser.py
+++ dataFuser.py
@@ -16,6 +16,7 @@ dir_anom = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/temp_anom'
 dir_coral = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/coral_snapshot'
 dir_neo = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/neo_data'
 dir_calipso = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/2021272175244_62409/'
+dir_geo = '/Users/josh/Google Drive/Georgia Tech Notes/Capstone/data/'
 
 bound_top = 26.405982971191
 bound_left = -82.882919311523
@@ -23,9 +24,40 @@ bound_right = -79.850692749023
 bound_bottom = 24.208717346191
 
 df_anom = process_images(dir_anom, 'temp_anom', bound_top, bound_left, bound_bottom, bound_right, False)
+df_anom['Date'] = pd.to_datetime(df_anom['Date']) - MonthBegin(1)
+
 df_coral = process_images(dir_coral, 'coral_algae', bound_top, bound_left, bound_bottom, bound_right, True)
 df_neo = process_neo(dir_neo, bound_top, bound_left, bound_bottom, bound_right)
 df_sat = process_sat(dir_calipso)
+
+df_seagrass = process_images("{}/{}".format(dir_geo, 'seagrass'), 'seagrass',
+                             bound_top, bound_left, bound_bottom, bound_right, False)
+df_rock = process_images("{}/{}".format(dir_geo, 'rock'), 'rock',
+                         bound_top, bound_left, bound_bottom, bound_right, False)
+df_rubble = process_images("{}/{}".format(dir_geo, 'rubble'), 'rubble',
+                           bound_top, bound_left, bound_bottom, bound_right, False)
+df_sand = process_images("{}/{}".format(dir_geo, 'sand'), 'sand',
+                         bound_top, bound_left, bound_bottom, bound_right, False)
+df_reef_slope = process_images("{}/{}".format(dir_geo, 'reef_slope'), 'reef_slope',
+                               bound_top, bound_left, bound_bottom, bound_right, False)
+df_sheltered_reef_slope = process_images("{}/{}".format(dir_geo, 'sheltered_reef_slope'), 'sheltered_reef_slope',
+                                         bound_top, bound_left, bound_bottom, bound_right, False)
+df_outer_reef_flat = process_images("{}/{}".format(dir_geo, 'outer_reef_flat'), 'outer_reef_flat',
+                                    bound_top, bound_left, bound_bottom, bound_right, False)
+df_reef_crest = process_images("{}/{}".format(dir_geo, 'reef_crest'), 'reef_crest',
+                               bound_top, bound_left, bound_bottom, bound_right, False)
+df_inner_reef_flat = process_images("{}/{}".format(dir_geo, 'inner_reef_flat'), 'inner_reef_flat',
+                                    bound_top, bound_left, bound_bottom, bound_right, False)
+df_terrestrial_reef_flat = process_images("{}/{}".format(dir_geo, 'terrestrial_reef_flat'), 'terrestrial_reef_flat',
+                                          bound_top, bound_left, bound_bottom, bound_right, False)
+df_plateau = process_images("{}/{}".format(dir_geo, 'plateau'), 'plateau',
+                            bound_top, bound_left, bound_bottom, bound_right, False)
+df_backreef = process_images("{}/{}".format(dir_geo, 'backreef'), 'backreef',
+                             bound_top, bound_left, bound_bottom, bound_right, False)
+df_shallow_lagoon = process_images("{}/{}".format(dir_geo, 'shallow_lagoon'), 'shallow_lagoon',
+                                   bound_top, bound_left, bound_bottom, bound_right, False)
+df_deep_lagoon = process_images("{}/{}".format(dir_geo, 'deep_lagoon'), 'deep_lagoon',
+                                bound_top, bound_left, bound_bottom, bound_right, False)
 
 
 def aggregate_data(df_main, metric):
@@ -35,9 +67,6 @@ def aggregate_data(df_main, metric):
     df['Lat'] = pd.to_numeric(df.Lat)
     df['Lat'] = np.round(df.Lat, 4)
     return df.groupby(['Lat', 'Long', 'Date']).agg(X=(metric, 'median')).reset_index()
-
-
-df_anom['Date'] = pd.to_datetime(df_anom['Date']) - MonthBegin(1)
 
 
 def create_gdf(df):
@@ -51,6 +80,21 @@ gdf_coral = create_gdf(df_coral)
 gdf_neo = create_gdf(df_neo)
 gdf_sat = create_gdf(df_sat)
 
+gdf_seagrass = create_gdf(df_seagrass)
+gdf_rock = create_gdf(df_rock)
+gdf_rubble = create_gdf(df_rubble)
+gdf_sand = create_gdf(df_sand)
+gdf_reef_slope = create_gdf(df_reef_slope)
+gdf_sheltered_reef_slop = create_gdf(df_sheltered_reef_slope)
+gdf_outer_reef_flat = create_gdf(df_outer_reef_flat)
+gdf_reef_crest = create_gdf(df_reef_crest)
+gdf_inner_reef_flat = create_gdf(df_inner_reef_flat)
+gdf_terrestrial_reef_flat = create_gdf(df_terrestrial_reef_flat)
+gdf_plateau = create_gdf(df_plateau)
+gdf_backreef = create_gdf(df_backreef)
+gdf_shallow_lagoon = create_gdf(df_shallow_lagoon)
+gdf_deep_lagoon = create_gdf(df_deep_lagoon)
+
 # gdf_anom.to_csv('temp_anom.csv')
 # gdf_coral.to_csv('coral.csv')
 # gdf_neo.to_csv('neo_data.csv')
@@ -59,7 +103,6 @@ gdf_sat = create_gdf(df_sat)
 
 # https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point-in-other-dataframe
 def ckdnearest(gdA, gdB):
-
     nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
     nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
     btree = cKDTree(nB)
