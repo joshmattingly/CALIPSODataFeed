af1e13069b9856c69c5867141830caee19639063
Josh Mattingly <josh.mattingly@conagra.com>
2021-11-02 18:41:31 -0500

    competition framework

--- modelCompetitionFramework.py
+++ modelCompetitionFramework.py
@@ -29,7 +29,23 @@ class ModelCompetition:
         self.normalize = True
         self.scores = []
         self.models = {}
+        username = input("Username: ")
+        password = getpass.getpass(prompt='Password: ', stream=None)
+        if tunnel:
+            server = SSHTunnelForwarder(
+                ('pace-ice.pace.gatech.edu', 22),
+                ssh_username=username,
+                ssh_password=password,
+                remote_bind_address=('127.0.0.1', 5432)
+            )
+            server.start()
+            local_port = str(server.local_bind_port)
+            engine = create_engine('postgresql://{}@{}:{}/{}'.format(username, "127.0.0.1",
+                                                                     local_port, "coral_data"))
+        else:
+            engine = create_engine('postgresql://{}@localhost:5432/coral_data'.format(username))
 
+        # TODO: Convert train/test initializers to postgis tables
         self.X_train = self.train_set.loc[:, self.train_set.columns != self.response_variable]
         self.X_train_norm = (self.X_train-self.X_train.mean())/self.X_train.std()
         self.y_train = self.train_set[self.response_variable]
@@ -37,20 +53,6 @@ class ModelCompetition:
         self.X_test = self.test_set.loc[:, self.test_set.columns != self.response_variable]
         self.X_test_norm = (self.X_test-self.X_test.mean())/self.X_test.std()
         self.y_test = self.test_set[self.response_variable]
-
-        if tunnel:
-            server = SSHTunnelForwarder(
-                ('pace-ice.pace.gatech.edu', 22),
-                ssh_username=input("Username: "),
-                ssh_password=getpass.getpass(prompt='Password: ', stream=None),
-                remote_bind_address=('127.0.0.1', 5432)
-            )
-            server.start()
-            local_port = str(server.local_bind_port)
-            engine = create_engine('postgresql://{}@{}:{}/{}'.format("jmattingly31", "127.0.0.1",
-                                                                     local_port, "coral_data"))
-        else:
-            engine = create_engine('postgresql://jmattingly31@localhost:5432/coral_data')
 
     def random_forest(self):
         return 0
